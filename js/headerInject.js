856393cab45145ca7c8781d1c96b9e9c7fc9febb
Areef ur Rahman <aareefurrahman@gmail.com>
2025-06-21 03:01:10 +0500

    mobile menu injection fix

--- js/headerInject.js
+++
@@ -1,8 +0,0 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     let header = document.getElementById("student-header");
-//     let header_in_Home = document.getElementById("student-header-in-Home");
-
-//     if (header && header_in_Home) {
-//         header_in_Home.appendChild(header);
-//     }
-// });
