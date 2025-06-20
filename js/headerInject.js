eb1e9556cd4568a799ac8a5fdb058758ce6dcaaa
Areef ur Rahman <aareefurrahman@gmail.com>
2025-06-21 02:55:59 +0500

    Header inject in Home page

---
+++ js/headerInject.js
@@ -0,0 +1,8 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     let header = document.getElementById("student-header");
+//     let header_in_Home = document.getElementById("student-header-in-Home");
+
+//     if (header && header_in_Home) {
+//         header_in_Home.appendChild(header);
+//     }
+// });
